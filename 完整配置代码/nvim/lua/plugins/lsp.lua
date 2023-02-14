b3616a5fc1412a860515a662723429116514974e
dansoncut <42341074+dansoncut@users.noreply.github.com>
2023-02-14 15:29:38 +0800

    sumneko_lua.setup也要修改
    如果需要配置，sumneko_lua.setup改为lua_ls

--- 完整配置代码/nvim/lua/plugins/lsp.lua
+++ 完整配置代码/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,6 @@ require("mason-lspconfig").setup({
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
 }
