ee2eb4a40c0b663c4cf96c452c32d317867de17a
eggtoopain <42341074+eggtoopain@users.noreply.github.com>
2023-05-30 11:21:24 +0800

    更新treesitter.lua
    “help”需要改为"vimdoc"，不然在使用lazy的时候会出错

--- 完整配置代码/nvim/lua/plugins/treesitter.lua
+++ 完整配置代码/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- 添加不同语言
-  ensure_installed = { "vim", "help", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+  ensure_installed = { "vim", "vimdoc", "bash", "c", "cpp", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
 
   highlight = { enable = true },
   indent = { enable = true },
